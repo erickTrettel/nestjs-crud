3f7329e4315958e53194134dc8eeedcf3df14670
Erick Trettel <ericktrettel@gmail.com>
2022-03-19 19:25:16 -0300

    Initial commit + config mongodb

---
+++ src/app.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { UsersModule } from './users/users.module';
+
+import { MongooseModule } from '@nestjs/mongoose';
+
+@Module({
+  imports: [
+    UsersModule,
+    MongooseModule.forRoot(
+      'mongodb+srv://admin:LcJaNJqgcSVcbF7@cluster0.36y3u.mongodb.net/test',
+    ),
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
